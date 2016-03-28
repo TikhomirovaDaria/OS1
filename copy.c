09c349c380ce472c33e2cadbe6dd5efd3c605f17
Tikhomirova Daria <tikhomirova.dg@tversu.ru>
2016-03-28 20:48:43 +0300

    Added CreateProcess function

--- copy.c
+++ copy.c
@@ -1,6 +1,8 @@
 #include <unistd.h>
 #include <stdio.h>
 
+pid_t CreateProcess(int n);
+
 int main () {
 	char* source = (char*)malloc(1024);
 	char* dest = (char*)malloc(1024);
@@ -14,4 +16,14 @@ int main () {
 	free(sourse);
 	free(dest);
 	return 0;
+}
+
+// Initial process creates exactly n child-processes
+pid_t CreateProcess(int n) {
+	pid_t pid = fork();
+    	for (int i=0; i < n-1 && pid > 0; i++){
+       		pid = fork();
+
+    	}
+    	return pid;
 }
