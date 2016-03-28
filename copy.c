aaf41b59fcdfdb159373bc0a19811affc54c3783
Tikhomirova Daria <tikhomirova.dg@tversu.ru>
2016-03-28 21:14:02 +0300

    Added CopyFile function

--- copy.c
+++ copy.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 pid_t CreateProcess(int n);
+int CopyFile (char* source, char* dest);
 
 int main () {
 	char* source = (char*)malloc(1024);
@@ -26,4 +27,22 @@ pid_t CreateProcess(int n) {
 
     	}
     	return pid;
+}
+
+// Copy 1 file
+int CopyFile (char* source, char* dest) {
+    	int in, out;
+    	char* buff = (char*)malloc(1024);
+    	int nread;
+    	in = open(source, O_RDONLY);
+    	out = open (dest, O_WRONLY|O_CREAT, S_IRWXU|S_IRWXO);
+    	if (!in || !out)
+        	return -1;
+   	 while ((nread = read(in, buff, sizeof(block))) > 0)
+        	write(out,block,nread);
+
+   	close(out);
+    	close(in);
+    	free(buff);
+    	return 0;
 }
